08111287ecb20f3410b053491d20ec2ef279b0c7
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-03-01 16:22:56 -0300

    Initial commit

---
+++ BreakOut.py
@@ -0,0 +1,37 @@
+import turtle
+
+win = turtle.Screen()
+win.title("BreakOut by @Angelion879")
+win.bgcolor("black")
+win.setup(width=800, height=700)
+win.tracer(0)
+
+class Paddle(turtle.Turtle):
+    def __init__(self,position):
+        super().__init__(shape='square')
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.speed(0)
+        self.penup()
+        self.color('white')
+        self.goto(position)
+class Ball(turtle.Turtle):
+    def __init__(self,position):
+        super().__init__(shape='circle')
+        self.speed(0)
+        self.penup()
+        self.color('white')
+        self.goto(position)
+class Brick(turtle.Turtle):
+    def __init__(self,position,color):
+        super().__init__(shape='square')
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.speed(0)
+        self.penup()
+        self.color(color)
+        self.goto(position)
+
+paddle = Paddle((0, -300))
+ball = Ball((0,-279))
+
+while True:
+    win.update()
