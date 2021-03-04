864d9ea20e339ed0714887315c748683c0e71b76
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-03-04 15:21:02 -0300

    first brick line

--- walls.py
+++ walls.py
@@ -1,13 +1,15 @@
 import turtle
 
 class Brick(turtle.Turtle):
-    def __init__(self,position,color):
+    def __init__(self,position):
         super().__init__(shape='square')
         self.shapesize(stretch_wid=1, stretch_len=3)
         self.speed(0)
         self.penup()
-        self.color(color)
+        self.color('white')
         self.goto(position)
 
-brick = Brick((-375, 340), 'white')
-brick2 = Brick((-310, 340), 'white')
+#brick = Brick((-360, 340))
+
+for i in range(-360, 390, 65):
+    brick = Brick((i, 340))
